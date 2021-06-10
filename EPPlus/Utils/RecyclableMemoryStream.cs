21463999ba0b2bbdc3c439d536a5a3552db4924c
HueiFeng <695979933@qq.com>
2021-06-10 18:52:33 +0800

    Update RecyclableMemoryStream

--- EPPlus/Utils/RecyclableMemoryStream.cs
+++ EPPlus/Utils/RecyclableMemoryStream.cs
@@ -6,12 +6,23 @@ namespace OfficeOpenXml.Utils
 {
     public static class RecyclableMemoryStream
     {
-
         private static readonly Lazy<RecyclableMemoryStreamManager> recyclableMemoryStreamManager = new Lazy<RecyclableMemoryStreamManager>();
-        private static RecyclableMemoryStreamManager RecyclableMemoryStreamManager => recyclableMemoryStreamManager.Value;
+        private static RecyclableMemoryStreamManager RecyclableMemoryStreamManager
+        {
+            get
+            {
+                var recyclableMemoryStream = recyclableMemoryStreamManager.Value;
+                recyclableMemoryStream.MaximumFreeSmallPoolBytes = 64 * 1024 * 1024;
+                recyclableMemoryStream.MaximumFreeLargePoolBytes = 64 * 1024 * 32;
+                recyclableMemoryStream.AggressiveBufferReturn = true;
+                return recyclableMemoryStream;
+            }
+        }
+        private const string TagSource = "Magicodes.EPPlus";
+
         internal static MemoryStream GetStream()
         {
-            return RecyclableMemoryStreamManager.GetStream();
+            return RecyclableMemoryStreamManager.GetStream(TagSource);
         }
 
         internal static MemoryStream GetStream(byte[] array)
@@ -21,7 +32,7 @@ namespace OfficeOpenXml.Utils
 
         internal static MemoryStream GetStream(int capacity)
         {
-            return RecyclableMemoryStreamManager.GetStream(null, capacity);
+            return RecyclableMemoryStreamManager.GetStream(TagSource, capacity);
         }
     }
 }
